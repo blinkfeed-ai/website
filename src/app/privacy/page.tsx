c9c500f99e7e3668d1d42d99415d53c2fe6b6871
BinarySoftware <mkmikolajek@icloud.com>
2024-05-27 22:44:06 +0200

    save

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import PrivacyPolicyMarkdown from './privacy-policy.md';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export default function Privacy() {
+  return (
+    <main 
+    className="relative flex min-h-screen flex-col justify-between" 
+    style={{
+        padding: '1rem',
+        maxWidth: '800px',
+        gap: '1rem',
+        margin: '0 auto'
+      }}
+    >
+      <ReactMarkdown children={PrivacyPolicyMarkdown} />
+    </main>
+  );
+}
