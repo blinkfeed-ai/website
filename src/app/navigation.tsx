9ff4fb2359701b2f1e98f38519bd148918cf9657
BinarySoftware <mkmikolajek@icloud.com>
2024-06-10 16:27:06 +0200

    Hamburger menu

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,6 +1,10 @@
-import {Bars3Icon} from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import {Container} from '@/app/Container'
+import { Dialog } from '@headlessui/react'
+
 import * as Button from '@/app/Button'
+
+import React from 'react'
 
 const items = [
   {name: 'Blinkfeed', href: '#'},
@@ -8,6 +12,52 @@ const items = [
   {name: 'Community', href: 'https://discord.gg/NmqDddBV', target: '_blank'},
   {name: 'Feature Request', href: 'https://blinkfeed.featurebase.app', target: '_blank'},
 ]
+
+export default function HamburgerMenu() {
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false)
+
+  return (
+    <header className="bg-white w-full lg:w-auto">
+      <nav className="mx-auto flex flex-row w-full lg:w-auto" aria-label="Global">
+        <div className="flex lg:hidden mr-auto">
+          <button
+            type="button"
+            className="-ml-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Logo />
+      </nav>
+      <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-white px-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="m-4 mt-2 flow-root">
+            <div className="divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+              <div className="mb-6 text-white">
+                  <Button.SignUp />
+                </div>
+                {/* TODO: Close menu on click */}
+                {items.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
 
 function Logo() {
   return (
@@ -59,7 +109,7 @@ function Items() {
 
 function Rhs() {
   return (
-    <div className='flex items-center gap-x-6 text-white'>
+    <div className='hidden lg:flex items-center gap-x-6 text-white'>
       <Button.SignUp />
     </div>
   )
@@ -71,11 +121,11 @@ export function Navigation() {
   return (
     <>
       <div className='h-4' />
-      <header className='sticky top-0 bg-page z-50'>
+      <header className='sticky top-0 bg-page z-40'>
         <nav>
           <Container>
-            <div className='flex items-center gap-12' style={{height: `${HEIGHT}px`}}>
-              <Logo />
+            <div className='flex items-center lg:gap-12' style={{height: `${HEIGHT}px`}}>
+              <HamburgerMenu />
               <div className='flex-grow flex justify-between items-center'>
                 <Items />
                 <Rhs />
