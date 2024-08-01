10857e82e4e95dcee3bd1fcdee75108107f0e2fc
BinarySoftware <mkmikolajek@icloud.com>
2024-08-01 20:37:32 +0200

    Add social media + contacts

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,11 +6,12 @@ const navigation = {
     // {name: 'Community', href: 'https://discord.gg/NmqDddBV'},
     {name: 'Feature Request', href: 'https://blinkfeed.featurebase.app/'},
     {name: 'Privacy', href: 'privacy'},
+    {name: 'Contact', href: 'mailto:maciej@blinkfeed.ai'},
   ],
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.facebook.com/blinkfeedAi/',
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
           <path
@@ -23,7 +24,7 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: 'https://www.instagram.com/blinkfeed_ai/',
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
           <path
@@ -36,7 +37,7 @@ const navigation = {
     },
     {
       name: 'X',
-      href: '#',
+      href: 'https://x.com/blinkfeed_ai',
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
           <path d='M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z' />
@@ -44,21 +45,26 @@ const navigation = {
       ),
     },
     {
-      name: 'GitHub',
-      href: '#',
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/blinkfeed/',
       icon: (props: React.SVGProps<SVGSVGElement>) => (
-        <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
-          <path
-            fillRule='evenodd'
-            d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
-            clipRule='evenodd'
-          />
+        <svg fill='currentColor' viewBox='0 0 17 17' {...props}>
+          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>        
+        </svg>
+      ),
+    },
+    {
+      name: 'TikTok',
+      href: 'https://www.tiktok.com/@blinkfeed_ai',
+      icon: (props: React.SVGProps<SVGSVGElement>) => (
+        <svg fill='currentColor' viewBox='0 0 17 17' {...props}>
+          <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
         </svg>
       ),
     },
     {
       name: 'YouTube',
-      href: '#',
+      href: 'https://www.youtube.com/channel/UCtoJ9bLqZaMgOcXElnntuvg',
       icon: (props: React.SVGProps<SVGSVGElement>) => (
         <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
           <path
@@ -92,19 +98,19 @@ export function Footer() {
             )
           })}
         </nav>
-        {/*<div className='mt-10 flex justify-center space-x-10'>*/}
-        {/*  {navigation.social.map(item => (*/}
-        {/*    <a*/}
-        {/*      key={item.name}*/}
-        {/*      href={item.href}*/}
-        {/*      target='_blank'*/}
-        {/*      className='text-gray-400 hover:text-gray-500'*/}
-        {/*    >*/}
-        {/*      <span className='sr-only'>{item.name}</span>*/}
-        {/*      <item.icon className='h-6 w-6' aria-hidden='true' />*/}
-        {/*    </a>*/}
-        {/*  ))}*/}
-        {/*</div>*/}
+        <div className='mt-10 flex justify-center space-x-10'>
+          {navigation.social.map(item => (
+            <a
+              key={item.name}
+              href={item.href}
+              target='_blank'
+              className='text-gray-400 hover:text-gray-500'
+            >
+              <span className='sr-only'>{item.name}</span>
+              <item.icon className='h-6 w-6' aria-hidden='true' />
+            </a>
+          ))}
+        </div>
         <p className='mt-10 text-center text-xs leading-5 text-secondary'>
           &copy; {new Date().getFullYear()} Blinkfeed. All rights reserved.
         </p>
